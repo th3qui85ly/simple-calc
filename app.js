eb222e7f22303380f95506d4f7604a1eacfffc23
= <=>
2022-04-16 22:21:49 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const add = require("./add");
+const subtract = require("./subtract");
+const multiply = require("./multiply");
+const divide = require("./divide");
+
+console.log("Add : " + add(4,5));
+console.log("Subtract : " + subtract(5,4));
+console.log("Multiply : " + multiply(4,5));
+console.log("Divide : " + divide(80,5));
